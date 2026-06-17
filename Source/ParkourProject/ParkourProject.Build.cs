48a7b2b8a12fa68a8d44b3dd6af5bb1b0c9dc657
Stefano Gismano <stefano.gismano@gmail.com>
2026-06-17 11:38:41 +0200

    Initial commit

---
+++ Source/ParkourProject/ParkourProject.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ParkourProject : ModuleRules
+{
+	public ParkourProject(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"ParkourProject",
+			"ParkourProject/Variant_Horror",
+			"ParkourProject/Variant_Horror/UI",
+			"ParkourProject/Variant_Shooter",
+			"ParkourProject/Variant_Shooter/AI",
+			"ParkourProject/Variant_Shooter/UI",
+			"ParkourProject/Variant_Shooter/Weapons"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
